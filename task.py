9839413330c219271b5bc3c91c567d99809507b5
jamshid1598 <dovurovjamshid982@gmail.com>
2022-09-07 22:34:50 +0500

    celery config file created and changed some configuration

--- task.py
+++ task.py
@@ -1,7 +1,28 @@
 from celery import Celery
+import time
 
-app = Celery('tasks', backend="rpc://", broker='pyamqp://')
+app = Celery('tasks', backend="rpc://", broker='pyamqp://guest@localhost//')
+
+# changing task_serializer
+app.conf.task_serializer = 'json'
+
+# configuring many settings at once
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],  # Ignore other content
+    result_serializer='json',
+    timezone='Asia/Tashkent',
+    enable_utc=True,
+)
+
+# load changed configurations from manually created module
+# app.config_from_object('celeryconfig')
+
 
 @app.task
 def add(x,y):
+    y *= 1000
+    while not x > y:
+        x += 1
+        time.sleep(0.001)
     return x + y
